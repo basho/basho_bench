a370c4c89ecc4a5015d15b8871292f5018c6706e
Dave Smith <dizzyd@dizzyd.com>
2009-12-08 14:33:04 -0700

    Adding skeleton of worker; stats system

--- src/riak_bench_worker.erl
+++ src/riak_bench_worker.erl
@@ -19,7 +19,7 @@
 %% under the License.    
 %%
 %% -------------------------------------------------------------------
--module(riak_bench_stats).
+-module(riak_bench_worker).
 
 -behaviour(gen_server).
 
@@ -39,9 +39,6 @@
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-op_complete(Op, Result, ElapsedUs) ->
-    gen_server:cast(?MODULE, {op, Op, Result, ElapsedUs}).
-
 %% ====================================================================
 %% gen_server callbacks
 %% ====================================================================
@@ -53,9 +50,7 @@ handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
-handle_cast({op, Op, ok, ElapsedUs}, State) ->
-    {noreply, State};
-handle_cast({op, Op, {error, Reason}, ElapsedUs}, State) ->
+handle_cast(_Msg, State) ->
     {noreply, State}.
 
 handle_info(_Info, State) ->
@@ -66,5 +61,4 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
 
