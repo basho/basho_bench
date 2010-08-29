4cf3de6e04d43552f87615515c23477d7e513b81
Dave Smith <dizzyd@dizzyd.com>
2010-08-29 14:53:21 -0600

    Fixing typo in function header

--- src/basho_bench_valgen.erl
+++ src/basho_bench_valgen.erl
@@ -36,7 +36,7 @@ new({fixed_bin, Size}, _Id) ->
 new({exponential_bin, MinSize, Mean}, _Id) ->
     Source = init_source(),
     fun() -> data_block(Source, MinSize + trunc(stats_rv:exponential(1 / Mean))) end;
-new({uniform_bin, MinSize, MaxSize}) ->
+new({uniform_bin, MinSize, MaxSize}, _Id) ->
     Source = init_source(),
     Diff = MaxSize - MinSize,
     fun() -> data_block(Source, MinSize + random:uniform(Diff)) end;
