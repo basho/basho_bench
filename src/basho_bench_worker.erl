2f9f84e91889f8f455f65e987c3a906540f0bf87
Ryan Zezeski <rzezeski@gmail.com>
2012-09-08 16:42:01 -0400

    Replace calls to basho_stats
    The basho_stats repo was recently replaced by folsom but these two
    calls were missed.

--- src/basho_bench_worker.erl
+++ src/basho_bench_worker.erl
@@ -293,7 +293,7 @@ max_worker_run_loop(State) ->
 rate_worker_run_loop(State, Lambda) ->
     %% Delay between runs using exponentially distributed delays to mimic
     %% queue.
-    timer:sleep(trunc(basho_stats_rv:exponential(Lambda))),
+    timer:sleep(trunc(basho_bench_stats:exponential(Lambda))),
     case worker_next_op(State) of
         {ok, State2} ->
             case needs_shutdown(State2) of
