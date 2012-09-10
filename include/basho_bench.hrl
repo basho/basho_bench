c85c7596a81922afa9f191598222542aa36c3590
Dave Parfitt <dparfitt@basho.com>
2012-09-10 13:54:31 -0400

    fix: warn -> warning

--- include/basho_bench.hrl
+++ include/basho_bench.hrl
@@ -5,7 +5,7 @@
 
 -define(DEBUG(Str, Args), lager:debug(Str, Args)).
 -define(INFO(Str, Args), lager:info(Str, Args)).
--define(WARN(Str, Args), lager:warn(Str, Args)).
+-define(WARN(Str, Args), lager:warning(Str, Args)).
 -define(ERROR(Str, Args), lager:error(Str, Args)).
 
 -define(FMT(Str, Args), lists:flatten(io_lib:format(Str, Args))).
