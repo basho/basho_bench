3f4704013ca3d5961aee792b58455e718558b1b1
Mathieu Lecarme <mlecarme@bearstech.com>
2013-05-20 12:27:16 +0200

    More realistic scenario.

--- src/basho_bench_driver_carbon.erl
+++ src/basho_bench_driver_carbon.erl
@@ -6,25 +6,61 @@
 
 -record(state, {
         host,
-        port
+        port,
+        keep_connect,
+        batch_size,
+        socket
     }).
 
 new(_Id) ->
     Host = basho_bench_config:get(carbon_server, "127.0.0.1"),
     Port = basho_bench_config:get(carbon_port, 2003),
-    {ok, #state{host=Host, port=Port}}.
+    KeepConnect = basho_bench_config:get(carbon_keep_connect, false),
+    BatchSize = basho_bench_config:get(carbon_batch_size, 100),
+    {ok, #state{
+            host=Host,
+            port=Port,
+            keep_connect=KeepConnect,
+            batch_size=BatchSize,
+            socket=nil}}.
 
-run(set, KeyGen, _ValueGen, State) ->
-    case gen_tcp:connect(State#state.host,
-                         State#state.port,
-                         [list, {packet, 0}]) of
+carbon(State = #state{host=Host, port=Port, socket=nil}, Message) ->
+    case gen_tcp:connect(Host, Port, [list, {packet, 0}]) of
         {ok, Sock} ->
-            {Mega, Sec, _Micro} = now(),
-            Msg = io_lib:format("pim.pam.poum.~p ~p ~p~n", [KeyGen(), (Mega * 1000 + Sec),  random:uniform(1000)]),
-            gen_tcp:send(Sock, Msg),
-            gen_tcp:close(Sock),
-            {ok, State};
+            carbon(State#state{socket=Sock}, Message);
         Error ->
-            {error, Error, State}
+            Error
+    end;
+
+carbon(State = #state{socket=Socket, keep_connect=KeepConnect}, Message) ->
+    case gen_tcp:send(Socket, Message) of
+        ok ->
+            case KeepConnect of
+                true ->
+                    {ok, State};
+                _ ->
+                    ok = gen_tcp:close(Socket),
+                    {ok, State#state{socket=nil}}
+            end;
+        Error ->
+            Error % let it crash
+    end.
+
+concat(0, _Keygen, _Ts, List) ->
+    List;
+
+concat(Size, KeyGen, Ts, List) ->
+    Msg = io_lib:format("pim.pam.poum.~p ~p ~p~n", [KeyGen(), Ts,  random:uniform(1000)]),
+    concat(Size -1, KeyGen, Ts, [Msg | List]).
+
+run(set, KeyGen, _ValueGen, State = #state{batch_size=BatchSize}) ->
+    {Mega, Sec, _Micro} = now(),
+    Msg = concat(BatchSize, KeyGen, Mega * 1000 + Sec, []),
+
+    case carbon(State, Msg) of
+        {error, E} ->
+            {error, E, State};
+        OK ->
+            OK
     end.
 
