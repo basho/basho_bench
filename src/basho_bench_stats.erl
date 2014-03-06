d8f34b57e6b7229a9017e995a9f6c05c05d61b96
Tomasz Lichon <xorver@gmail.com>
2014-03-06 20:20:45 +0100

    loop log fix

--- src/basho_bench_stats.erl
+++ src/basho_bench_stats.erl
@@ -33,6 +33,8 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-export([loop_log/0]).
+
 -include("basho_bench.hrl").
 
 -record(state, { ops,
