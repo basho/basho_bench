36a580cc18d750d1dbddce9644ede98541a2696a
Jon Glick <jglick@basho.com>
2014-05-03 12:40:52 -0400

    Fixed uneven length error

--- priv/combine.r
+++ priv/combine.r
@@ -17,7 +17,6 @@ compute$errors      = compute$sum
 # Columns to average when combining (left for default):
 # n, min, mean, median, 95th, 99th, 99_9th, max, errors
 
-
 # Make results directory
 dir.create(file.path("./", "combined"), showWarnings = FALSE)
 
@@ -26,19 +25,32 @@ list_dirs <- list.files("./", pattern = "test[0-9][0-9]?")
 
 # Peek to find the set of CSV files
 files <- list.files(paste(list_dirs[[1]], "/tests/current/", sep=""), pattern = "csv")
+#files <- list("summary.csv")
 
 combine_filetype = function(file_name){
 
     print(paste("Compiling", file_name))
 
     # Read csv files as an array of matrices
-    list_data <- lapply(list_dirs, function(x) {
+    lengths <- list() # abind doesn't work on uneven lists,
+                      #   so we grab the length
+    list_df <- lapply(list_dirs, function(x) {
         file <- paste(x, "/tests/current/",file_name, sep="")
         df <- read.csv(file)
-        data.matrix(df)
+        lengths[length(lengths)+1] <<- length(df[[1]]) # cooky <<- for breaking scope
+        lengths <- c(lengths, length(df))
+        df
+    })
+    maxlen = min(unlist(lengths))
+    if (maxlen <= 0) {
+        print(paste("Min length of", file_name, "<= 0, skipping"))
+        return (FALSE)
+    }
+
+    list_data <- lapply(list_df, function(x) {
+        data.matrix(x[1:maxlen,]) # returning only maxlen of the data frames
     })
 
-    # Combine the list of matrices to a 3d matrix
     data <- abind(list_data, along=3)
 
     # For each column name...
@@ -61,5 +73,3 @@ combine_filetype = function(file_name){
 
 # for each *.csv file type, combine
 res <- sapply(files, combine_filetype)
-
-#combine_filetype("short.csv")
