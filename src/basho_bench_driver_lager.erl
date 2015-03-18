9ca133d5f1fc8dfe7b99d050ffdfc18c57da612a
Mark Allen <mallen@basho.com>
2015-03-18 13:14:43 -0500

    WIP lager driver

---
+++ src/basho_bench_driver_lager.erl
@@ -0,0 +1,61 @@
+%% -------------------------------------------------------------------
+%%
+%% basho_bench: Benchmarking Suite
+%%
+%% Copyright (c) 2015 Basho Techonologies
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+-module(basho_bench_driver_lager).
+
+-export([new/1,
+         run/4]).
+
+-export([sink_generator/0, lager_msg_generator/0]).
+
+-include("basho_bench.hrl").
+
+-record(state, {
+          multiple_sink_support = false
+    }).
+
+%% ====================================================================
+%% API
+%% ====================================================================
+
+new(_ID) ->
+    {ok, #state{multiple_sink_support = erlang:function_exported(lager, log, 5)}}.
+
+run(log, SinkGen, ValueGen, State = #state{multiple_sink_support = S}) ->
+    Sink = SinkGen(),
+    {Level, Metadata, Format, Args} = ValueGen(),
+    Result = case S of
+        true -> 
+            lager:log(Sink, Level, Metadata, Format, Args);
+        false ->
+            lager:log(Level, Metadata, Format, Args)
+    end,
+    case Result of 
+        ok -> {ok, State};
+        {error, lager_not_running} -> {'EXIT', lager_not_running};
+        {error, Reason} -> {error, Reason, State}
+    end.
+
+sink_generator() ->
+    ok.
+
+lager_msg_generator() ->
+    ok.
