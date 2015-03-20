2d90f2f61a4afa3b70512d7a4ca8f1c4959fb7fa
Mark Allen <mallen@basho.com>
2015-03-19 21:40:21 -0500

    Flesh out random data generation

--- src/basho_bench_driver_lager.erl
+++ src/basho_bench_driver_lager.erl
@@ -27,6 +27,7 @@
 -export([keygen/1, valgen/1, sink_generator/0, lager_msg_generator/0]).
 
 -include("basho_bench.hrl").
+-include_lib("lager/include/lager.hrl").
 
 -record(state, {
           multiple_sink_support = false
@@ -37,18 +38,30 @@
 %% ====================================================================
 
 new(_ID) ->
+    %% I guess it's mildly evil to use the process dictionary to store
+    %% stateful things...
+
+    Sinks = basho_bench_config:get(lager_sinks, []),
+    erlang:put(lager_sinks, Sinks),
+
+    Levels = basho_bench_config:get(lager_levels, []),
+    case Levels == [] of
+        true -> erlang:put(lager_levels, ?LEVELS);
+        false -> erlang:put(lager_levels, Levels)
+    end,
+
     {ok, #state{multiple_sink_support = erlang:function_exported(lager, log, 5)}}.
 
 run(log, SinkGen, ValueGen, State = #state{multiple_sink_support = S}) ->
     Sink = SinkGen(),
     {Level, Metadata, Format, Args} = ValueGen(),
     Result = case S of
-        true -> 
+        true ->
             lager:log(Sink, Level, Metadata, Format, Args);
         false ->
             lager:log(Level, Metadata, Format, Args)
     end,
-    case Result of 
+    case Result of
         ok -> {ok, State};
         {error, lager_not_running} -> {'EXIT', lager_not_running};
         {error, Reason} -> {error, Reason, State}
@@ -60,10 +73,41 @@ keygen(_Id) ->
 valgen(_Id) ->
     fun lager_msg_generator/0.
 
-%% XXX FIXME: obviously a placeholder
 sink_generator() ->
-    ok.
+    Sinks = erlang:get(lager_sinks),
+    get_random(Sinks, lager_event). % TODO - this is hard coded for now because DEFAULT_SINK macro is only in the multiple-sink branch
 
-%% XXX FIXME: obviously a placeholder
 lager_msg_generator() ->
-    {info, [], "~p", [<<"foo">>]}.
+    Level = get_random(erlang:get(lager_levels), debug),
+    Metadata = maybe_generate_metadata(),
+    Args = maybe_generate_args(),
+    Fmt = generate_fmt(Args),
+    {Level, Metadata, Fmt, Args}.
+
+get_random(List, Default) ->
+    Len = length(List),
+    case Len of
+        0 -> Default;
+        1 -> hd(List);
+        _ -> lists:nth(random:uniform(Len), List)
+    end.
+
+maybe_generate_metadata() ->
+    [].
+
+maybe_generate_args() ->
+    NumArgs = random:uniform(6) - 1,
+    generate_args(NumArgs, []).
+
+generate_args(0, Acc) -> lists:reverse(Acc);
+generate_args(N, Acc) ->
+    Char = random:uniform(26) + 64, % 64 precedes ASCII "A" (65), so this will generate a char in the range of A-Z
+    Num  = random:uniform(100),
+    generate_args(N - 1, [ list_to_binary(string:chars(Char, Num)) | Acc ]).
+
+generate_fmt(Args) ->
+    L = length(Args),
+    case L of
+        0 -> "No arguments!";
+        _ -> string:copies("~p ", L)
+    end.
