d8e5e50c665d4bcd5cda8cf335469d9ac3d9320d
Benjamin Bastian <benjamin.bastian@gmail.com>
2016-02-12 11:19:53 -0800

    Change basho_bench_sup supervision tree
    basho_bench_sup now monitors basho_bench_config, which is now
    long-lived. It also monitors basho_bench_run_sups, which are dynamically
    spawned and handle all the logic for a particular benchmark run.

--- src/basho_bench_sup.erl
+++ src/basho_bench_sup.erl
@@ -23,66 +23,28 @@
 
 -behaviour(supervisor).
 
-%% API
--export([start_link/0,
-         workers/0,
-         stop_child/1]).
+-export([start_link/0]).
 
-%% Supervisor callbacks
 -export([init/1]).
 
--include("basho_bench.hrl").
+-export([start_child/0]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 30000, Type, [I]}).
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
-%% ===================================================================
-%% API functions
-%% ===================================================================
+
+start_child() ->
+    RunSpec = {
+        basho_bench_run_sup,
+        {basho_bench_run_sup, start_link, []},
+        temporary,
+        5000,
+        worker,
+        [basho_bench_run_sup]
+    },
+    supervisor:start_child(?MODULE, RunSpec).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-workers() ->
-    [Pid || {_Id, Pid, worker, [basho_bench_worker]} <- supervisor:which_children(?MODULE)].
-
-stop_child(Id) ->
-    ok = supervisor:terminate_child(?MODULE, Id),
-    ok = supervisor:delete_child(?MODULE, Id).
-
-
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
-
 init([]) ->
-    %% Get the number concurrent workers we're expecting and generate child
-    %% specs for each
-
-    %% intentionally left in to show where worker profiling start/stop calls go.
-    %% eprof:start(),
-    %% eprof:start_profiling([self()]),
-    Workers = worker_specs(basho_bench_config:get(concurrent), []),
-    MeasurementDriver =
-        case basho_bench_config:get(measurement_driver, undefined) of
-            undefined -> [];
-            _Driver -> [?CHILD(basho_bench_measurement, worker)]
-        end,
-
-    {ok, {{one_for_one, 5, 10},
-        [?CHILD(basho_bench_stats, worker)] ++
-        Workers ++
-        MeasurementDriver
-    }}.
-
-%% ===================================================================
-%% Internal functions
-%% ===================================================================
-
-worker_specs(0, Acc) ->
-    Acc;
-worker_specs(Count, Acc) ->
-    Id = list_to_atom(lists:concat(['basho_bench_worker_', Count])),
-    Spec = {Id, {basho_bench_worker, start_link, [Id, Count]},
-            permanent, 30000, worker, [basho_bench_worker]},
-    worker_specs(Count-1, [Spec | Acc]).
+    {ok, {{one_for_one, 5, 10}, [?CHILD(basho_bench_config, worker)]}}.
