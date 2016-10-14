0e32b95a775665b8b5efbaec3eabb0b6120dfd05
russelldb <russelldb@r2s12.bos1>
2016-10-14 04:17:43 -0400

    WIP: name partions of int for reset

--- src/basho_bench_driver_bigset.erl
+++ src/basho_bench_driver_bigset.erl
@@ -34,6 +34,7 @@
 
 -record(state, { client,
                  last_key=undefined,
+                 set_val_gen_name = undefined,
                  remove_set, %% The set name to perform a remove on
                  remove_ctx, %% The context of a get from `remove_set'
                  remove_value, %% The values from a get to `remove_set'
@@ -58,6 +59,7 @@ new(Id) ->
     Cookie  = basho_bench_config:get(bigset_cookie, 'bigset'),
     MyNode  = basho_bench_config:get(bigset_mynode, [basho_bench, longnames]),
     BatchSize = basho_bench_config:get(bigset_batchsize, 1000),
+    SetValGen = basho_bench_config:get(bigset_valgen_name, a),
 
     %% Try to spin up net_kernel
     case net_kernel:start(MyNode) of
@@ -83,21 +85,21 @@ new(Id) ->
         {error, Reason2} ->
             ?FAIL_MSG("Failed get a bigset_client to ~p: ~p\n", [TargetNode, Reason2]);
         Client ->
-            {ok, #state { client = Client, batch_size=BatchSize }}
+            {ok, #state { client = Client, batch_size=BatchSize,
+                          set_val_gen_name=SetValGen }}
     end.
 
 run(read, KeyGen, _ValueGen, State) ->
     Key = KeyGen(),
     #state{client=C} = State,
-    ?DEBUG("Read ~p~n", [Key]),
+
     case bigset_client:read(Key, [], C) of
         {ok, {ctx, Ctx}, {elems, Set}} ->
-            %% Store the latest Ctx/State for a remove
-            RemoveVal = random_element(Set),
-            {ok, State#state{remove_set=Key, remove_ctx=Ctx, remove_value=RemoveVal}};
+       	  {ok, State};
         {error, not_found} ->
             {ok, State};
         {error, Reason} ->
+	     ?FAIL_MSG("error on read ~p~n", [Reason]),
             {error, Reason, State}
     end;
 run(insert, KeyGen, ValueGen, State) ->
@@ -138,7 +140,8 @@ run(insert_pl, KeyGen, ValueGen, State) ->
             {error, Reason, State2}
     end;
 run(batch_insert, KeyGen, ValueGen, State) ->
-    #state{client=C, batch_size=BatchSize, last_key=LastKey0} = State,
+    #state{client=C, batch_size=BatchSize, last_key=LastKey0,
+           set_val_gen_name=SVGN} = State,
 
     {Set, Members} = case {LastKey0, gen_members(BatchSize, ValueGen)} of
                          {_, []} ->
@@ -146,7 +149,7 @@ run(batch_insert, KeyGen, ValueGen, State) ->
                              Key = KeyGen(),
 
                              ?DEBUG("New set after exhausted ~p~n", [Key]),
-                             basho_bench_keygen:reset_sequential_int_state(),
+                             basho_bench_keygen:reset_sequential_int_state(SVGN),
                              {Key, gen_members(BatchSize, ValueGen)};
                          {undefined, List} ->
                              %% We have no active set, so generate a
