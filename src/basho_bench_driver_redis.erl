63b9de66eaa71e622a31f0ef4c0b8686599dacd3
James Gorlick <jgorlick@basho.com>
2016-11-14 16:46:20 -0800

    for redis driver, added invalid traffic generator, added retries in a configurable manner to deal w/ b_b causing itself issues by generating invalid traffic.

--- src/basho_bench_driver_redis.erl
+++ src/basho_bench_driver_redis.erl
@@ -26,11 +26,13 @@
 
 -include("basho_bench.hrl").
 
--record(state, { c, bucket }).
+-record(state, { c, bucket, retries, retry_delay }).
 
 new(Id) ->
 	Ips  = basho_bench_config:get(redis_ips, ["127.0.0.1"]),
 	Port  = basho_bench_config:get(redis_port, 6379),
+    Retries = basho_bench_config:get(retries, 3),
+    RetryDelay = basho_bench_config:get(retry_delay, 10),
 	Targets = basho_bench_config:normalize_ips(Ips, Port),
 	Bucket  = basho_bench_config:get(bucket_name, "my-bucket"),
 	{TargetIp, TargetPort} = lists:nth((Id rem length(Targets)+1), Targets),
@@ -39,8 +41,10 @@ new(Id) ->
 	case eredis:start_link(TargetIp, TargetPort) of
 		{ok, C} ->
 			{ok, #state { 
-				c = C, 
-				bucket = Bucket
+                    c = C,
+                    bucket = Bucket,
+                    retries = Retries,
+                    retry_delay = RetryDelay
 			}};
 		{error, Reason} ->
 			?FAIL_MSG("Failed to connect to redis at ~p:~p: ~p\n", [TargetIp, TargetPort, Reason])
@@ -50,27 +54,37 @@ get_key(KeyGen, #state{bucket = Bucket}) ->
     KeyNum = integer_to_list(KeyGen()),
     lists:flatten([Bucket, ":", KeyNum]).
 
+execute_w_retries(Connection, Command, State, 0) ->
+    case eredis:q(Connection, Command) of
+        {ok, _} -> {ok, State};
+        {error, Reason} -> {error, Reason, State}
+    end;
+execute_w_retries(Connection, Command, State = #state{retry_delay = RetryDelay},
+                  Retries) ->
+    case eredis:q(Connection, Command) of
+        {ok, _} -> {ok, State};
+        {error, _Reason} ->
+            timer:sleep(RetryDelay),
+            execute_w_retries(Connection, Command, State, Retries - 1)
+    end.
+
 run(set, KeyGen, ValueGen, State) ->
     Key = get_key(KeyGen, State),
-	case eredis:q(State#state.c, ["SET", Key, ValueGen()]) of
-		{ok, _} -> 
-			{ok, State};
-		{error, Reason} -> 
-			{error, Reason, State}
-	end;
+    execute_w_retries(State#state.c, ["SET", Key, ValueGen()], State,
+                      State#state.retries);
 run(del, KeyGen, _ValueGen, State) ->
     Key = get_key(KeyGen, State),
-	case eredis:q(State#state.c, ["DEL", Key]) of
-		{ok, _} -> 
-			{ok, State};
-		{error, Reason} -> 
-			{error, Reason, State}
-	end;
+    execute_w_retries(State#state.c, ["DEL", Key], State,
+                      State#state.retries);
 run(get, KeyGen, _ValueGen, State) ->
     Key = get_key(KeyGen, State),
-	case eredis:q(State#state.c, ["GET", Key]) of
-		{ok, _} ->
-			{ok, State};
-		{error, Reason} ->
-			{error, Reason, State}
-	end.
+    execute_w_retries(State#state.c, ["GET", Key], State,
+                      State#state.retries);
+run(invalid, KeyGen, _ValueGen, State) ->
+    Keys = [get_key(KeyGen, State), get_key(KeyGen, State)],
+    case eredis:q(State#state.c, ["INVALID", Keys]) of
+        {ok, _} ->
+            {error, "Invalid command succeeded", State};
+        {error, _Reason} ->
+            {ok, State}
+    end.
